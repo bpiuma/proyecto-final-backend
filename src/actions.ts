92ec52492e4464ebe30db5f6c393f3c84f0b8819
Andres Campopiano <acampopiano@gmail.com>
2021-06-01 17:37:27 +0000

    subo login y logout

--- src/actions.ts
+++ src/actions.ts
@@ -2,6 +2,9 @@ import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'  // getRepository"  traer una tabla de la base de datos asociada al objeto
 import { User } from './entities/User'
 import { Exception } from './utils'
+import jwt from 'jsonwebtoken'
+import fetch from 'cross-fetch'
+import extend from 'extend'
 
 export const createUser = async (req: Request, res:Response): Promise<Response> =>{
 
@@ -24,4 +27,32 @@ export const createUser = async (req: Request, res:Response): Promise<Response> 
 export const getUsers = async (req: Request, res: Response): Promise<Response> =>{
 		const users = await getRepository(User).find();
 		return res.json(users);
+}
+
+export const login = async (req: Request, res: Response): Promise<Response> => {
+    let { email, password } = req.body;
+    if (!email) throw new Exception("Please specify an email on your request body", 400)
+    if (!password) throw new Exception("Please specify a password on your request body", 400)
+    if (!validateEmail(email)) throw new Exception("Please provide a valid email address", 400)
+    
+    const userRepo = getRepository(User)
+    const user = await userRepo.findOne({ where: { email } })
+    if (!user) throw new Exception("Invalid email", 401)
+    if (!user.checkIfUnencryptedPasswordIsValid(password)) throw new Exception("Invalid password", 401)
+    const token = jwt.sign({ user }, process.env.JWT_KEY as string, { expiresIn: process.env.JWT_TOKEN_EXPIRE_IN});
+    res.cookie('currentUser', email);        
+    return res.cookie('auth-token', token, {httpOnly: true, path:'/', domain: 'localhost'}).json({ user, token });
+}
+
+
+export const logout = async (req: Request, res: Response) => {
+   // req.session.destroy();
+    res.status(202).clearCookie('currentUser')
+    res.status(202).clearCookie('auth-token').send('Success logged out')
+    
+}
+
+const validateEmail = (email: string) => {
+    const res = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return res.test(email);
 }
